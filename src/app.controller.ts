1d12e53ede0e6c3389a1da434cc1fee4a2bc6f72
Cristian Cotes <cotes.cristian@gmail.com>
2018-04-29 17:43:24 +0200

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,58 @@
+import { Get, Controller } from '@nestjs/common';
+import * as rc from 'typed-rest-client/HttpClient';
+
+// const url: string = 'https://api.fotocasa.es/PropertySearch/Search?combinedLocationIds=724,9,8,232,376,8019,0,1146,0&culture=es-ES&hrefLangCultures=ca-ES%3Bes-ES%3Bde-DE%3Ben-GB&isNewConstruction=false&isMap=false&latitude=41.3854&longitude=2.17754&maxPrice=325000&minRooms=2&minSurface=60&pageNumber=1&platformId=1&propertySubtypeIds=6&propertyTypeId=2&sortOrderDesc=true&sortType=bumpdate&transactionTypeId=1';
+
+@Controller()
+export class AppController {
+  @Get()
+  async root(): string {
+    const rest: rc.RestClient = new rc.HttpClient('rest-samples');
+
+    // -H 'origin: https://www.fotocasa.es'
+    // -H 'accept-encoding: gzip, deflate, br'
+    // -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8'
+    // -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    // -H 'accept: application/json, text/plain, */*'
+    const headers: any = {
+      'origin': 'https://www.fotocasa.es',
+      'accept-encoding': 'deflate, br',
+      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+      'accept': 'application/json, text/plain, */*'
+    };
+    const url: string = 'https://api.fotocasa.es/PropertySearch/Search?' +
+      'combinedLocationIds=724,9,8,232,376,8019,0,1146,0&' +
+      'culture=es-ES&' +
+      'hrefLangCultures=ca-ES%3Bes-ES%3Bde-DE%3Ben-GB&' +
+      'isNewConstruction=false&' +
+      'isMap=false&' +
+      'latitude=41.3854&' +
+      'longitude=2.17754&' +
+      'maxPrice=325000&' +
+      'minRooms=2&' +
+      'minSurface=60&' +
+      // 'pageNumber=1&' +
+      'platformId=1&' +
+      // 'propertySubtypeIds=6&' +
+      'propertyTypeId=2&' +
+      'sortOrderDesc=true&' +
+      'sortType=bumpdate&' +
+      'transactionTypeId=1';
+    let res: rm.HttpClientResponse = await rest.get(url, headers);
+
+    let body = await res.readBody();
+    let information = JSON.parse(body);
+    console.log(information.count);
+    let nextPage = 2;
+    const total = information.count;
+    for (let i=information.realEstates.length; i<total; i+=information.realEstates.length) {
+      const newUrl = url.concat(`&pageNumber=${nextPage}`);
+      res = await rest.get(newUrl, headers);
+      body = await res.readBody();
+      information = JSON.parse(body);
+      nextPage++;
+    }
+    return 'Hello World!';
+  }
+}
