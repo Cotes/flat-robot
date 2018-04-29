96b462e1a3c7f177b87219d115b35ea151613064
Cristian Cotes <cotes.cristian@gmail.com>
2018-04-29 18:19:58 +0200

    Create flat model and parse from API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller} from '@nestjs/common';
 import * as rc from 'typed-rest-client/HttpClient';
+import {Flat} from "./domain/flat.model";
 
-// const url: string = 'https://api.fotocasa.es/PropertySearch/Search?combinedLocationIds=724,9,8,232,376,8019,0,1146,0&culture=es-ES&hrefLangCultures=ca-ES%3Bes-ES%3Bde-DE%3Ben-GB&isNewConstruction=false&isMap=false&latitude=41.3854&longitude=2.17754&maxPrice=325000&minRooms=2&minSurface=60&pageNumber=1&platformId=1&propertySubtypeIds=6&propertyTypeId=2&sortOrderDesc=true&sortType=bumpdate&transactionTypeId=1';
 
 @Controller()
 export class AppController {
@@ -9,11 +9,6 @@ export class AppController {
   async root(): string {
     const rest: rc.RestClient = new rc.HttpClient('rest-samples');
 
-    // -H 'origin: https://www.fotocasa.es'
-    // -H 'accept-encoding: gzip, deflate, br'
-    // -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8'
-    // -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-    // -H 'accept: application/json, text/plain, */*'
     const headers: any = {
       'origin': 'https://www.fotocasa.es',
       'accept-encoding': 'deflate, br',
@@ -43,16 +38,31 @@ export class AppController {
 
     let body = await res.readBody();
     let information = JSON.parse(body);
-    console.log(information.count);
+    this.parseFlats(information);
     let nextPage = 2;
     const total = information.count;
-    for (let i=information.realEstates.length; i<total; i+=information.realEstates.length) {
+    for (let i = information.realEstates.length; i < total; i += information.realEstates.length) {
       const newUrl = url.concat(`&pageNumber=${nextPage}`);
       res = await rest.get(newUrl, headers);
       body = await res.readBody();
       information = JSON.parse(body);
+      this.parseFlats(information);
       nextPage++;
     }
     return 'Hello World!';
+  }
+
+  private parseFlats(information: any): Flat[] {
+    if (!information.realEstates) {
+      return [];
+    }
+    const flats = information.realEstates.map(
+      flat => Flat.create(
+        flat.id,
+        flat.description,
+        flat.features,
+        flat.multimedias.map(m => m.url)
+      ));
+    return flats;
   }
 }
---
+++ src/domain/flat.model.ts
@@ -0,0 +1,15 @@
+// (c) Telefonica Innovación Alpha. All rights reserved
+
+export class Flat {
+
+  constructor(
+    private providerId: number,
+    private description: string,
+    private features: any,
+    private images: string[]
+  ) {}
+
+  static create(providerId: number, description: string, features: any, images: string[]) {
+    return new Flat(providerId, description, features, images);
+  }
+}
